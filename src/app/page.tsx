4d28c3b6c12d44087ac42f0b08b22e0e1617d34a
VibhorTanwar <tanwarvikas42@gmail.com>
2025-10-04 19:02:43 +0530

    footer error corrected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,9 @@ import ClubLeadership from "@/components/ClubLeadership";
 import PhotoGallery from "@/components/PhotoGallery";
 import ChatBot from "@/components/ChatBot";
 import BePartSection from "@/components/BePartSection";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 import { motion } from "framer-motion";
 import groupPhoto from "../../public/groupPhoto.jpg";
-import { g } from "framer-motion/client";
 import Mentors from "@/components/Mentors";
 const inter = Inter({
   subsets: ['latin'],
